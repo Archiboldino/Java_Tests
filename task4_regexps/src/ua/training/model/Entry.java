2e2c636baae129f06c17858bd0d40b6b20376428
Nikita Zabaykin <vladlihovid@gmail.com>
2017-11-10 14:57:56 +0200

    Implemented Entry class

--- task4_regexps/src/ua/training/model/Entry.java
+++ task4_regexps/src/ua/training/model/Entry.java
@@ -1,11 +1,44 @@
 package ua.training.model;
 
 /**
- * Model
+ * Entry
  * created on 10.11.2017
  *
  * @author Nikita Zabaykin vladlihovid@gmail.com
  * @version 1.0
  */
-public class  {
+public class Entry {
+    private String name;
+    private String login;
+    private String comment;
+
+    public Entry(String name, String login, String comment) {
+        this.name = name;
+        this.login = login;
+        this.comment = comment;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
