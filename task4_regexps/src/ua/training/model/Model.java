8eaafd776379022deacff8e5827a656f4c38f85c
Nikita Zabaykin <vladlihovid@gmail.com>
2017-11-10 14:58:23 +0200

    Added functions to Model class

--- task4_regexps/src/ua/training/model/Model.java
+++ task4_regexps/src/ua/training/model/Model.java
@@ -1,5 +1,8 @@
 package ua.training.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Model
  * created on 10.11.2017
@@ -8,4 +11,9 @@ package ua.training.model;
  * @version 1.0
  */
 public class Model {
+    private List<Entry> entries = new ArrayList<>();
+
+    public void addEntry(Entry entry) {
+        entries.add(entry);
+    }
 }
