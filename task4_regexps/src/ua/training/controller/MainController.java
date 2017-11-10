c1a7571c6f1a159c8bf3dd929fe0fe88ade3f7da
Nikita Zabaykin <vladlihovid@gmail.com>
2017-11-10 14:50:38 +0200

    Added MVC files

--- task4_regexps/src/ua/training/controller/MainController.java
+++ task4_regexps/src/ua/training/controller/MainController.java
@@ -1,13 +1,11 @@
 package ua.training.main;
 
 /**
- * Main
- * created on 08.11.2017
+ * MainController
+ * created on 10.11.2017
  *
  * @author Nikita Zabaykin vladlihovid@gmail.com
  * @version 1.0
  */
-public class Main {
-    public static void main(String[] args) {
-    }
+public class MainController {
 }
---
+++ task4_regexps/src/ua/training/model/Entry.java
@@ -0,0 +1,11 @@
+package ua.training.model;
+
+/**
+ * Model
+ * created on 10.11.2017
+ *
+ * @author Nikita Zabaykin vladlihovid@gmail.com
+ * @version 1.0
+ */
+public class  {
+}
