65afea83ac3167e34f14fcd5c6755612b1ced909
Nikita Zabaykin <vladlihovid@gmail.com>
2017-11-13 18:28:00 +0200

    Added new Exception

---
+++ task4_regexps/src/ua/training/exceptions/NotUniqueException.java
@@ -0,0 +1,11 @@
+package ua.training.exceptions;
+
+/**
+ * NotUniqueExceptoin
+ * created on 13.11.2017
+ *
+ * @author Nikita Zabaykin vladlihovid@gmail.com
+ * @version 1.0
+ */
+public class NotUniqueExceptoin {
+}
