7f86ab9bd4ca2ae9b6d5fe9b7e2a2a4f3f24e893
Nikita Zabaykin <vladlihovid@gmail.com>
2017-11-16 15:51:24 +0200

    Added builder usage exapmle

--- DriverCarIntr/src/com/company/Main.java
+++ DriverCarIntr/src/com/company/Main.java
@@ -1,24 +1,18 @@
 package com.company;
 
 import com.company.model.entities.*;
-import com.company.model.service.Driver;
 
 import java.time.LocalDate;
-import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        Human john = new Human("John" , LocalDate.now() , Human.Gender.MALE , Human.BloodGroup.FIRST);
+        Human john = Human.getBuilder()
+                    .name("John the Builder")
+                    .birthDate(LocalDate.now())
+                    .bloodGroup(Human.BloodGroup.FIRST)
+                    .gender(Human.Gender.MALE).build();
 
-        DriverLicense license = new DriverLicense(john,
-                LocalDate.now(),
-                Arrays.asList(DriverLicense.Category.A, DriverLicense.Category.D),
-                LocalDate.of(1,2,14));
-
-        Driver driverJohn = new Driver(john, license);
-
-        System.out.println(driverJohn);
-        System.out.println(driverJohn.getHuman().gender());
+        System.out.println(john);
     }
 }
